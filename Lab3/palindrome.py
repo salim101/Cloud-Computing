ba5f7ed807b57a6bcc849ceda4d00a6a84839503
salimuddin <salim.uddin@mydit.ie>
2015-09-30 15:14:03 +0000

    added comment to the program

--- Lab3/palindrome.py
+++ Lab3/palindrome.py
@@ -1,10 +1,13 @@
+# take an input from the user
 inp = raw_input('Enter something to check for palindrome: ')
 
+#use the built in reversed function in python
 rev=reversed (inp)
 
+#if statement to check the input for palindrome
 if list (inp) == list(rev):
-  print 'true'
-else:
+  print 'true' #if it matches print true
+else: # otherwise print false
   print 'false'
 
 
